69c9e8bf16fe82aa03809689172ff7c535d32fdd
Andrew Birnberg <andrew.birnberg@gmail.com>
2018-01-05 22:16:01 +0000

    Fixed imports

--- web_flask.py
+++ web_flask.py
@@ -2,7 +2,8 @@
 """URL web application"""
 
 import base62
-from urlparse import urlparse
+from urllib.parse import urlparse
+from datetime import datetime
 from flask import Flask, request, redirect, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -16,7 +17,7 @@ db = SQLAlchemy(app)
 class Url(db.Model):
     short = db.Column(db.String, primary_key=True)
     original = db.Column(db.String(80), unique=True, nullable=False)
-    created_at = Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
 
     def __repr__(self):
         return '<Short URL: {}\nActual URL: {}>'.format(self.short,
@@ -28,20 +29,22 @@ db.create_all()
 def page_not_found(e):
     return render_template('web_static/404.html'), 404
 
-@app.route('/', strict_slashes=False, methods=['POST'])
+@app.route('/', strict_slashes=False, methods=['GET', 'POST'])
 def add_url():
     """ validates url and returns shortend url """
-    originalUrl = request.form.get('ogUrl')
-    if urlparse(originalUrl).scheme == '':
-        originalUrl = 'http://' + originalUrl
+    if request.method == 'POST':
+        originalUrl = request.form.get('ogUrl')
+        if urlparse(originalUrl).scheme == '':
+            originalUrl = 'http://' + originalUrl
 
-    shortUrl = base62.encode(originalUrl)
+        shortUrl = base62.encode(originalUrl)
 
-    newurl = Url(short=shortUrl, original=originalUrl)
-    db.session.add(newurl)
-    db.session.commit()
+        newurl = Url(short=shortUrl, original=originalUrl)
+        db.session.add(newurl)
+        db.session.commit()
 
-    return jsonify({'shortUrl': shortUrl}), 201
+        return jsonify({'shortUrl': shortUrl}), 201
+    return render_template('web_static/404.html'), 404
 
 @app.route('/<url>', strict_slashes=False)
 def uri_handle(url):
@@ -54,4 +57,4 @@ def uri_handle(url):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='10.250.1.228')
